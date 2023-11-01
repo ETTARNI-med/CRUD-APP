e573e1d182b74ca46bdb854e6c8242c90fb86939
name <ettarni.hamada@gmail.com>
2023-11-01 15:15:53 +0100

    Start the html structure of te app

--- Server.js
+++ Server.js
@@ -7,8 +7,14 @@ const app =  express();
 require("dotenv").config();
 const port = process.env.PORT;
 
+//set the view engine
+app.set('view engine', 'ejs');
+
+//load assets
+
+
 app.get("/",(req, res) =>{
-    res.send("Crud Application")
+    res.render('index');
 })
 
 app.listen(port,()=> console.log(`Server is running on the port ${port}`))
